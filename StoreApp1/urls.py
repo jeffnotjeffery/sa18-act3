9f80ac965861f5042b39b11c984de958e93df28f
jeffnotjeffery <jeffery.hubbart@yahoo.com>
2024-04-30 02:22:13 -0500

    finished sa18

--- StoreApp1/urls.py
+++ StoreApp1/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views  # Import the views module from the 'myapp' app directory
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('<int:id>/', views.show, name='show'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
+import json
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def index(request):
+    with open('myapp/fixtures/products.json') as json_file:  # Corrected path to fixtures folder
+        products = json.load(json_file)
+    return render(request, 'myapp/index.html', {'products': products})
+
+def show(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'myapp/show.html', {'product': product})
